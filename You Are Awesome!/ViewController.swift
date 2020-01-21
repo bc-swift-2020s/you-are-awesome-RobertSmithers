258c7f1c8f2166dfd5179be801cf1f36876aff1c
Robert Smithers <rjsmithers3@gmail.com>
2020-01-20 21:42:56 -0500

    Completed 2.4

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,14 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Viewload did run!")
-        messageLabel.text = "Fabulous? That's you!"
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        print("Button was pressed!")
         messageLabel.text = "You Are Awesome!"
+        messageLabel.textColor = UIColor.blue
     }
     
+    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Great!"
+        messageLabel.textColor = .systemRed
+    }
 }
 
